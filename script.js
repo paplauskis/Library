9c01f628563dad776edad051e67027ad293b9ce9
paplauskis <paplauskaskostas@gmail.com>
2023-05-28 21:27:57 +0300

    changed object constructor to class

--- script.js
+++ script.js
@@ -6,35 +6,39 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  // Flips read status
+  toggleReadStatus() {
+    this.read = !this.read;
+  }
 }
-//flips read status
-Book.prototype.toggleReadStatus = function() {
-  this.read = !this.read;
-};
+
+submitButton.addEventListener('click', addBookToLibrary());
 
 function addBookToLibrary() {
   submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-  
+
     const titleValue = title.value.trim();
     const authorValue = author.value.trim();
     const pagesValue = pages.value.trim();
-    
     const newBook = new Book(titleValue, authorValue, pagesValue);
     myLibrary.push(newBook);
-    if (myLibrary.length > 1) myLibrary.shift();
+    if (myLibrary.length > 1) {
+      myLibrary.shift();
+    }
     displayBooks();
     form.reset();
     formDiv.style.display = 'none';
-  })
+  });
 }
-
-submitButton.addEventListener('click', addBookToLibrary());
 
 function displayBooks() {
   myLibrary.forEach((book) => {
@@ -50,9 +54,9 @@ function displayBooks() {
     toggleStatusButton.classList.add('toggle-status');
     toggleStatusButton.textContent = book.read ? 'Mark as not read' : 'Mark as read';
     toggleStatusButton.style.borderTopColor = book.read ? 'rgb(147, 255, 147)' : 'rgb(255, 136, 136)';
-    
+
     const gridDiv = document.querySelector('.container');
-    //toggles Book read/not read status
+    // Toggles Book read/not read status
     toggleStatusButton.addEventListener('click', () => {
       book.toggleReadStatus();
       toggleStatusButton.textContent = book.read ? 'Mark as not read' : 'Mark as read';
@@ -63,13 +67,15 @@ function displayBooks() {
     books.appendChild(removeBookButton);
     gridDiv.appendChild(books);
     newBookButton.textContent = 'Add Book';
-    //removes a book on click
+
+    // Removes a book on click
     removeBookButton.addEventListener('click', () => books.remove());
   });
-};
+}
 
 const formDiv = document.querySelector('.form');
 formDiv.style.display = 'none';
+
 const newBookButton = document.querySelector('.new-book');
 newBookButton.addEventListener('click', () => {
   if (formDiv.style.display === 'none') {
